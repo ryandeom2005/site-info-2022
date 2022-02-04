35324161e9434136b0986afe661a692721623f98
ryandeom <ryandeom@gmail.com>
2022-02-04 14:24:50 +0100

    php

---
+++ php/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div align="center">
+    <h2>inscription</h2>
+    <form method="POST" action="">
+    </br></br>
+    
+  
+
+    <table>
+        <tr>
+            <td align="right">
+            <label for="pseudo">Pseudo :</label>
+        </td>
+        <td>
+            <input id="pseudo" type="text" placeholder="Votre pseudo" name="pseudo">
+        </td>
+    </tr>
+    
+
+    <tr>
+        <td align="right">
+        <label for="mail">Adresse mail :</label>
+    </td>
+    <td>
+        <input id="mail" type="text" placeholder="Votre adresse mail" name="mail">
+    </td>
+</tr>
+
+
+
+<tr>
+    <td align="right">
+    <label for="mdp">Mot de passe :</label>
+</td>
+<td>
+    <input id="mdp" type="password" placeholder="Votre mot de passe" name="mdp">
+</td>
+</tr>
+
+<tr>
+    <td align="right">
+    <label for="mdp2">Confirmation du mot de passe :</label>
+</td>
+<td>
+    <input id="mdp2" type="password" placeholder="Confirmer votre mdp" name="mdp2">
+</td>
+</tr>
+
+<tr>
+    <td align="right">
+
+</td>
+
+<td>
+    <br/>
+    <input type="submit" value="Je m'inscris">
+</td>
+</tr>
+
+        </table>
+    </br>
+       
+    </form>
+    </div>
+</body>
+</html>
